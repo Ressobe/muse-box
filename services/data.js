5d530cc70f487cbd90f57c04c9e62559aafa23d5
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-02-04 21:39:54 +0100

    feat: finish spotify script to grab data

--- services/data.js
+++ services/data.js
@@ -1,149 +1,105 @@
-import { appendFileSync } from "node:fs";
+import { getToken, getAlbumInfo, getArtistAlbums } from './spotifyAPI.js';
+import db from "../src/lib/db.js"
 
-const file = "./artists.txt"
+const accessToken = await getToken();
 
-// check if artist in database first
-// for every artist i have to find all albumns (t1) and songs (t2)
-// create records in database for this artist
-//
-async function getToken() {
-  // Konfiguracja autoryzacji
-  const authString = `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`;
-  const base64AuthString = btoa(authString);
+async function createAlbums(artistID) {
+  const albumsData = await getArtistAlbums(accessToken, artistID);
+  let albums = [];
 
-  // Endpoint do uzyskania tokena dostępu
-  const tokenUrl = 'https://accounts.spotify.com/api/token';
+  if (albumsData.items && albumsData.items.length > 0) {
+    await Promise.all(albumsData.items.map(async item => {
+       if(item !== undefined) {
+          const albumInfo = await getAlbumInfo(accessToken, item.id);
 
-  // Parametry do żądania tokena dostępu
-  const tokenParams = {
-    grant_type: 'client_credentials',
-  };
+          const alb = {
+            title: albumInfo.name,
+            img: albumInfo.images[0].url,
+            release_date: albumInfo.release_date,
+            spotify_link: albumInfo.external_urls.spotify,
+            total_tracks: albumInfo.total_tracks,
+            songs: [],
+          }
 
-  // Nagłówki zawierające zakodowane dane autoryzacyjne
-  const tokenHeaders = {
-    Authorization: `Basic ${base64AuthString}`,
-    'Content-Type': 'application/x-www-form-urlencoded',
-  };
+          albumInfo.tracks.items.forEach(track => {
+              const t = {
+                title: track.name,
+                track_number: track.track_number,
+                spotify_link: track.external_urls.spotify,
+                duration_ms: track.duration_ms,
+              };
+              alb.songs.push(t);
+          })
 
-  const response = await fetch(tokenUrl, {
-    method: 'POST',
-    headers: tokenHeaders,
-    body: new URLSearchParams(tokenParams),
-  });
-
-  const data = await response.json();
-  return data.access_token;
+          albums.push(alb);
+       };
+      })
+    )
+  }
+  return albums;
 }
 
-const token = await getToken()
+async function createArtist(name, artistID) {
 
-
-// async function getPlaylistsId(accessToken) {
-//   const categoryId = 'hiphop';
-//   const playlistUrl = `https://api.spotify.com/v1/browse/categories/${categoryId}/playlists`;
-//   const playlistParams = {
-//     country: 'US',
-//     limit: 20,
-//   };
-//   const playlistHeaders = {
-//     Authorization: `Bearer ${accessToken}`,
-//   };
-//
-//   const response = await fetch(`${playlistUrl}?${new URLSearchParams(playlistParams)}`, {
-//     method: 'GET',
-//     headers: playlistHeaders,
-//   });
-//
-//   const playlistData = await response.json();
-//   if (playlistData.playlists && playlistData.playlists.items && playlistData.playlists.items.length > 0) {
-//     return playlistData.playlists.items;
-//   } else {
-//     throw new Error("Nie znaleziono playlisty w kategorii 'hipop'.");
-//   }
-// }
-//
-// const playlists = await getPlaylistsId(token)
-//
-//
-// async function saveArtists(artists) {
-//   for (let i = 0; i < artists.length; i++){
-//     const a = artists[i];
-//     await appendFileSync(file, `${a.name},${a.id}\n`, "utf8");
-//   }
-// }
-//
-// async function getTopArtists(accessToken, playlistId) {
-//   const tracksUrl = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
-//   const tracksParams = {
-//     limit: 50,
-//   };
-//   const playlistHeaders = {
-//     Authorization: `Bearer ${accessToken}`,
-//   };
-//
-//   const response = await fetch(`${tracksUrl}?${new URLSearchParams(tracksParams)}`, {
-//     method: 'GET',
-//     headers: playlistHeaders,
-//   });
-//
-//   const tracksData = await response.json();
-//
-//   // Sprawdzenie, czy 'items' jest zdefiniowane
-//   if (tracksData.items && tracksData.items.length > 0) {
-//     // Przetwarzanie informacji o utworach (np. wydobycie informacji o artystach)
-//     tracksData.items.forEach(async(item) => {
-//       await saveArtists(item.track.artists);
-//     });
-//   } else {
-//     console.error('Nie znaleziono utworów w playliście.');
-//   }
-// }
-//
-// playlists.forEach(async (item) => {
-//   await getTopArtists(token, item.id)
-// })
-
-const tacoID = "7CJgLPEqiIRuneZSolpawQ"
-
-async function getAlbumInfo(accessToken, albumId) {
-  const albumURL = `https://api.spotify.com/v1/albums/${albumId}`;
-  const albumHeaders = {
-    Authorization: `Bearer ${accessToken}`,
-  };
-
-  const response = await fetch(albumURL, {
-    method: 'GET',
-    headers: albumHeaders,
-  })    
-
-  const albumData = await response.json();
-
-  console.log(albumData.name)
-
-}
-
-async function getArtistAlbums(accessToken, artistID) {
-  const albumsURL = `https://api.spotify.com/v1/artists/${artistID}/albums`
-
-  const albumsHeaders = {
-    Authorization: `Bearer ${accessToken}`,
-  };
-
-  const response = await fetch(`${albumsURL}`, {
-    method: 'GET',
-    headers: albumsHeaders,
+  // Sprawdź, czy artysta już istnieje w bazie danych
+  const existingArtist = await prisma.artist.findUnique({
+    where: {
+      name: name,
+    },
   });
 
-  const albumsData = await response.json();
-
-  if (albumsData.items && albumsData.items.length > 0 ) {
-    albumsData.items.forEach(item => {
-      getAlbumInfo(accessToken, item.id);
-    })
+  if (existingArtist) {
+    console.log('Artysta już istnieje w bazie danych.');
+    return;
   }
 
-  return albumsData;
+  const albums = await createAlbums(artistID);
+
+ await prisma.artist.create({
+    data: {
+      name: name,
+      avg_rate: 0,
+      img: "img for artist",
+      albums: {
+        create: albums.map((album) => ({
+          title: album.title,
+          img: album.img,
+          // release_date: album.release_date,
+          total_tracks: album.total_tracks,
+          spotify_link: album.spotify_link,
+          songs: {
+            create: album.songs.map((song) => ({
+              title: song.title,
+              track_number: song.track_number,
+              spotify_link: song.spotify_link,
+              duration_ms: song.duration_ms,
+              genre: {
+                connectOrCreate: {
+                  where: { name: 'rap' }, // Tutaj możesz dostosować do swoich potrzeb
+                  create: { name: 'rap' },
+                },
+              },
+            }))
+          }
+        }))
+      },
+      spotify_id: artistID,
+    }})
 }
 
 
-await getArtistAlbums(token, tacoID);
+const path = './artists.txt';
+const file = Bun.file(path);
+const arrBuf = await file.arrayBuffer();
+
+const decoder = new TextDecoder();
+const str = decoder.decode(arrBuf);
+
+
+await Promise.all(str.split("\n").map(async item => {
+    const [name, spotifyId] = item.split(",");
+     if (name.length > 0) {
+        await createArtist(name, spotifyId);
+     };
+  })
+)

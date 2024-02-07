bf271c957bf21f1c80b02505b3839202566ac5c7
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-02-07 19:55:45 +0100

    feat: add ranking page

---
+++ src/app/ranking/loading.tsx
@@ -0,0 +1,5 @@
+export default function LoadingRanking() {
+    return (
+        <div>Load ranking</div>
+    );
+}
---
+++ src/app/ranking/page.tsx
@@ -0,0 +1,21 @@
+export default function Ranking() {
+    return (
+        <div className="flex flex-col items-center gap-10 justify-center mt-5">
+            <h1 className="text-3xl font-bold">Top 3 Artyści</h1>
+            <section>
+
+            </section>
+
+            <h1 className="text-3xl font-bold">Top 3 Albumy</h1>
+            <section>
+
+            </section>
+
+            <h1 className="text-3xl font-bold">Top 3 Piosenki</h1>
+            <section>
+
+            </section>
+
+        </div>
+    );
+}

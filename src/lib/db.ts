6d3b49cfa150d881b744b00de1dd652707a44ee3
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-02-17 21:15:19 +0100

    fix: fix problem with prisma client instances

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient();
-
-const globalForPrisma = global as unknown as { prisma: PrismaClient};
-
-export const db = globalForPrisma.prisma || prisma;
-
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
 
-export default db;
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma

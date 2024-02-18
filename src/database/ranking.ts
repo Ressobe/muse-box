0a190cdf5cef8707524ba84b670305782efdc636
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-02-18 14:52:26 +0100

    feat: add ranking prisma quiries

---
+++ src/database/ranking.ts
@@ -0,0 +1,29 @@
+import db from "@/src/lib/db";
+
+export async function getTopArtists() {
+    return await db.artist.findMany({
+        orderBy: {
+            popularity: 'desc'
+        },
+        take: 10,
+    })
+
+}
+
+export async function getTopRecordings() {
+    return await db.recording.findMany({
+        orderBy: {
+            popularity: 'desc'
+        },
+        take: 10,
+    })
+}
+
+export async function getTopTracks() {
+    return await db.track.findMany({
+        orderBy: {
+            popularity: 'desc'
+        },
+        take: 10,
+    })
+}

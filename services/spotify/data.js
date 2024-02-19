2bb8d86d53562b57d0e6eef1f2223cf8cbec0bdd
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-02-19 20:41:20 +0100

    refactor: move spotify api related files

---
+++ services/spotify/data.js
@@ -0,0 +1,109 @@
+import { getToken, getAlbumInfo, getArtistAlbums, getArtistInfo } from './spotifyAPI.js';
+import db from "../src/lib/db.js"
+
+const accessToken = await getToken();
+
+async function createAlbums(artistID) {
+  const albumsData = await getArtistAlbums(accessToken, artistID);
+  let albums = [];
+
+  if (albumsData.items && albumsData.items.length > 0) {
+    await Promise.all(albumsData.items.map(async item => {
+       if(item !== undefined) {
+          const albumInfo = await getAlbumInfo(accessToken, item.id);
+
+          const alb = {
+            title: albumInfo.name,
+            img: albumInfo.images[0].url,
+            release_date: albumInfo.release_date,
+            spotify_link: albumInfo.external_urls.spotify,
+            total_tracks: albumInfo.total_tracks,
+            songs: [],
+          }
+
+          albumInfo.tracks.items.forEach(track => {
+              const t = {
+                title: track.name,
+                track_number: track.track_number,
+                spotify_link: track.external_urls.spotify,
+                duration_ms: track.duration_ms,
+              };
+              alb.songs.push(t);
+          })
+
+          albums.push(alb);
+       };
+      })
+    )
+  }
+  return albums;
+}
+
+async function createArtist(name, artistID) {
+  // Sprawdź, czy artysta już istnieje w bazie danych
+  const existingArtist = await prisma.artist.findUnique({
+    where: {
+      name: name,
+    },
+  });
+
+  if (existingArtist) {
+    console.log('Artist exist in database');
+    return;
+  }
+
+  const artistData = await getArtistInfo(accessToken, artistID);
+  const albums = await createAlbums(artistID);
+
+
+ await prisma.artist.create({
+    data: {
+      nick: name,
+      avatar_img: artistData.images[0].url,
+      albums: {
+        create: albums.map((album) => ({
+          title: album.title,
+          img: album.img,
+          // release_date: album.release_date,
+          total_tracks: album.total_tracks,
+          spotify_link: album.spotify_link,
+          songs: {
+            create: album.songs.map((song) => ({
+              title: song.title,
+              track_number: song.track_number,
+              spotify_link: song.spotify_link,
+              duration_ms: song.duration_ms,
+              genre: {
+                connectOrCreate: {
+                  where: { name: 'rap' }, // Tutaj możesz dostosować do swoich potrzeb
+                  create: { name: 'rap' },
+                },
+              },
+            }))
+          }
+        }))
+      },
+      spotify_id: artistID,
+    }})
+}
+
+
+const path = './artists2.txt';
+const file = Bun.file(path);
+const arrBuf = await file.arrayBuffer();
+
+const decoder = new TextDecoder();
+const str = decoder.decode(arrBuf);
+
+
+const artistsData = str.split("\n").map(item => {
+  const [name, spotifyId] = item.split(",");
+  if (name.length > 0) {
+    return { name, spotifyId };
+  }
+}).filter(Boolean); // Usuń wartości null lub undefined
+
+for (const artistData of artistsData) {
+  await createArtist(artistData.name, artistData.spotifyId);
+  await new Promise(resolve => setTimeout(resolve, 5000));
+}
---
+++ services/spotify/spotifyAPI.js
@@ -0,0 +1,78 @@
+export async function getToken() {
+  // Konfiguracja autoryzacji
+  const authString = `${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`;
+  const base64AuthString = btoa(authString);
+
+  // Endpoint do uzyskania tokena dostępu
+  const tokenUrl = 'https://accounts.spotify.com/api/token';
+
+  // Parametry do żądania tokena dostępu
+  const tokenParams = {
+    grant_type: 'client_credentials',
+  };
+
+  // Nagłówki zawierające zakodowane dane autoryzacyjne
+  const tokenHeaders = {
+    Authorization: `Basic ${base64AuthString}`,
+    'Content-Type': 'application/x-www-form-urlencoded',
+  };
+
+  const response = await fetch(tokenUrl, {
+    method: 'POST',
+    headers: tokenHeaders,
+    body: new URLSearchParams(tokenParams),
+  });
+
+  const data = await response.json();
+  return data.access_token;
+}
+
+export async function getAlbumInfo(accessToken, albumId) {
+  const albumURL = `https://api.spotify.com/v1/albums/${albumId}`;
+  const albumHeaders = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  const response = await fetch(albumURL, {
+    method: 'GET',
+    headers: albumHeaders,
+    verbose: true,
+    timeout: 25000, // Przykładowy limit czasu w milisekundach
+  })    
+  const albumData = await response.json();
+  return albumData;
+}
+
+export async function getArtistAlbums(accessToken, artistID) {
+  const albumsURL = `https://api.spotify.com/v1/artists/${artistID}/albums`
+  const albumsHeaders = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  const response = await fetch(`${albumsURL}`, {
+    method: 'GET',
+    headers: albumsHeaders,
+    verbose: true,
+    timeout: 25000, // Przykładowy limit czasu w milisekundach
+  });
+
+  const albumsData = await response.json();
+  return albumsData;
+}
+
+export async function getArtistInfo(accessToken, artistID) {
+  const artistURL = `https://api.spotify.com/v1/artists/${artistID}`
+  const artistHeader = {
+    Authorization: `Bearer ${accessToken}`,
+  };
+
+  const response = await fetch(`${artistURL}`, {
+    method: 'GET',
+    headers: artistHeader,
+    verbose: true,
+    timeout: 25000, // Przykładowy limit czasu w milisekundach
+  });
+
+  const artistData = await response.json();
+  return artistData;
+}

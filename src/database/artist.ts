c2359863d8ff8f4567c3799d3a88b83cec3ec861
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-02-20 12:59:38 +0100

    feat: add follow and unfollow functions

--- src/database/artist.ts
+++ src/database/artist.ts
@@ -1,7 +1,7 @@
 import db from "@/src/lib/db";
 
-export async function getAllArtists() {
-    return await db.artist.findMany();
+export async function getAllArtists(take: number) { 
+    return await db.artist.findMany({take: take, include: {stats: true}});
 }
 
 export async function getArtist(artistId: string) {
@@ -19,6 +19,53 @@ export async function getArtist(artistId: string) {
         }
    });
 }
+
+export async function followArtist(artistId: string, profileId: string) {
+    await db.profile.update({
+        where: { id: profileId },
+        data: {
+            followedArtists: {
+                connect: {
+                    id: artistId
+                }
+            }
+        }
+    });
+    await db.artist.update({
+        where: {id : artistId},
+        data: {
+            stats: {
+                update: {
+                    amount_of_followers: {increment: 1}
+                }
+            }
+        }
+    });
+}
+
+export async function unfollowArtist(artistId: string, profileId: string) {
+    await db.profile.update({
+        where: { id: profileId },
+        data: {
+            followedArtists: {
+                disconnect: {
+                    id: artistId
+                }
+            }
+        }
+    });
+    await db.artist.update({
+        where: {id : artistId},
+        data: {
+            stats: {
+                update: {
+                    amount_of_followers: {decrement: 1}
+                }
+            }
+        }
+    });
+}
+
 
 export async function createArtistComment(artistId: string, creatorId: string, rate: number, comment: string) {
     await db.artist.update({

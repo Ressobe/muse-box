9d1201b5dd7b6217cc88374edf50c31acf395593
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-07-19 12:10:10 +0200

    feat: add edit and remove action

--- src/actions/reviews.ts
+++ src/actions/reviews.ts
@@ -3,7 +3,11 @@
 import { auth } from "@/auth";
 import { getUserArtistReview } from "@/data-access/user";
 import { Entity } from "@/types";
-import { createReviewUseCase } from "@/use-cases/review";
+import {
+  createReviewUseCase,
+  editReviewUseCase,
+  removeReviewUseCase,
+} from "@/use-cases/review";
 import { revalidatePath } from "next/cache";
 
 export async function addReviewAction(
@@ -29,6 +33,76 @@ export async function addReviewAction(
     }
   }
 
-  await createReviewUseCase(entityId, userId, comment, rating, type);
-  revalidatePath(`/artists/${entityId}`);
+  const review = await createReviewUseCase(
+    entityId,
+    userId,
+    comment,
+    rating,
+    type,
+  );
+
+  revalidatePath(`/${type}s/${entityId}`);
+
+  if (review) {
+    return { sucess: "review created!" };
+  }
+  return { error: "review not created!" };
+}
+
+export async function removeReviewAction(
+  entityId: string,
+  type: Entity,
+  ownerId: string,
+  commentId: string,
+) {
+  const session = await auth();
+  if (!session) {
+    return { error: "Not authorized access!" };
+  }
+
+  if (!session.user.id) {
+    return { error: "Not authorized access!" };
+  }
+
+  if (ownerId !== session.user.id) {
+    return { error: "Not authorized access!" };
+  }
+
+  await removeReviewUseCase(entityId, type, commentId);
+  revalidatePath(`/${type}s/${entityId}`);
+}
+
+export async function editReviewAction(
+  commentId: string,
+  entityId: string,
+  userId: string,
+  comment: string,
+  rating: number,
+  type: Entity,
+) {
+  const session = await auth();
+  if (!session) {
+    return { error: "Not authorized access!" };
+  }
+
+  if (!session.user.id) {
+    return { error: "Not authorized access!" };
+  }
+
+  const review = await editReviewUseCase(
+    commentId,
+    entityId,
+    userId,
+    comment,
+    rating,
+    type,
+  );
+
+  revalidatePath(`/${type}s/${entityId}`);
+
+  if (review) {
+    return { sucess: "review updated!" };
+  }
+
+  return { error: "review not updated!" };
 }

efd8498c9da6ac6a8435b648112795e6d16dda2d
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-07-19 12:25:55 +0200

    fix: comment update avg rating function

--- src/data-access/stat.ts
+++ src/data-access/stat.ts
@@ -11,31 +11,31 @@ const statsTables = {
 };
 
 export async function updateAverageRating(entityId: string, type: Entity) {
-  const table = reviewTables[type];
-  if (!table) {
-    throw new Error(`Unsupported entity type: ${type}`);
-  }
-
-  const result = await db
-    .select({
-      avgRating: db.raw("AVG(rating)"),
-      reviewCount: db.raw("COUNT(*)"),
-    })
-    .from(table)
-    .where(eq(table.entityId, entityId));
-
-  const { avgRating, reviewCount } = result[0];
-
-  const statsTable = statsTables[type];
-  if (!statsTable) {
-    throw new Error(`Unsupported entity type: ${type}`);
-  }
-
-  await db
-    .update(statsTable)
-    .set({
-      avgRating,
-      reviewCount,
-    })
-    .where(eq(statsTable.entityId, entityId));
+  // const table = reviewTables[type];
+  // if (!table) {
+  //   throw new Error(`Unsupported entity type: ${type}`);
+  // }
+  //
+  // const result = await db
+  //   .select({
+  //     avgRating: db.raw("AVG(rating)"),
+  //     reviewCount: db.raw("COUNT(*)"),
+  //   })
+  //   .from(table)
+  //   .where(eq(table.entityId, entityId));
+  //
+  // const { avgRating, reviewCount } = result[0];
+  //
+  // const statsTable = statsTables[type];
+  // if (!statsTable) {
+  //   throw new Error(`Unsupported entity type: ${type}`);
+  // }
+  //
+  // await db
+  //   .update(statsTable)
+  //   .set({
+  //     avgRating,
+  //     reviewCount,
+  //   })
+  //   .where(eq(statsTable.entityId, entityId));
 }

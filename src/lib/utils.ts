f1e943b74416b5a50d7beb37edbeed2b3c0d956e
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-07-19 12:22:23 +0200

    feat: add helpers function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,40 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function getYear(date: Date | null) {
+  if (!date) return "";
+  return new Date(date).getFullYear();
+}
+
+export function getTime(seconds: number | null) {
+  if (!seconds) return null;
+  // Calculate minutes and remaining seconds
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+
+  // Format the minutes and seconds to be two digits if needed
+  const formattedMinutes = minutes.toString();
+  const formattedSeconds =
+    remainingSeconds < 10
+      ? "0" + remainingSeconds
+      : remainingSeconds.toString();
+
+  // Combine and return the formatted time
+  return formattedMinutes + ":" + formattedSeconds;
+}
+
+export function getFullAlbumTime(
+  tracks: {
+    length: number | null;
+  }[],
+) {
+  let fullTime = 0;
+  for (const t of tracks) {
+    fullTime += t.length ?? 0;
+  }
+  return fullTime;
 }

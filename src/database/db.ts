799cb44ceab0a7799def9b9d52871d72f9e9562d
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-07-22 11:48:33 +0200

    fix

--- src/database/db.ts
+++ src/database/db.ts
@@ -3,9 +3,7 @@ import { createClient } from "@libsql/client";
 import * as schema from "./schema";
 
 const client = createClient({
-  url: "libsql://muse-box2-ressobe.turso.io",
-  authToken:
-    "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJhIjoicnciLCJpYXQiOjE3MjA1MTQzODUsImlkIjoiZTgxZDljYjctNWZhOS00YzdhLWIxYjYtOGNmY2NlNTRkYzFiIn0.pdM6Hi44eLGuy0QWmHVdhzS3r9b4twPOlGo2bQaHLazsoFjI4iESoOkI_CmbDnX5d5t2Hq4Gr5-Fp6tOv0UbDA",
+ 
 });
 
 const db = drizzle(client, { schema });

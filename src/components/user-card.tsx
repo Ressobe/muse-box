15d11547c1972382bc55ca78cae09a96d17eca21
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-08-13 12:24:55 +0200

    feat: add ability to search users profiles

---
+++ src/components/user-card.tsx
@@ -0,0 +1,31 @@
+import { FaUser } from "react-icons/fa";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import Link from "next/link";
+
+type UserCardProps = {
+  user: {
+    id: string;
+    name: string | null;
+    email: string;
+    image: string | null;
+  };
+};
+
+export function UserCard({ user }: UserCardProps) {
+  return (
+    <Link
+      href={`/profiles/${user.id}`}
+      className="flex flex-col items-center text-center cursor-pointer"
+    >
+      <div className="hover:bg-secondary/40 p-4 rounded transition-all">
+        <Avatar className="h-48 w-48 mb-2">
+          <AvatarImage src={user.image ?? ""} />
+          <AvatarFallback>
+            <FaUser className="w-24 h-24" />
+          </AvatarFallback>
+        </Avatar>
+        <span className="whitespace-normal">{user.name}</span>
+      </div>
+    </Link>
+  );
+}

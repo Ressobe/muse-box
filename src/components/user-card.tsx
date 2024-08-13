c489de283759b15390334f5c364baa070fcdde2f
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-08-13 15:41:52 +0200

    test: remove image with blur

--- src/components/user-card.tsx
+++ src/components/user-card.tsx
@@ -1,7 +1,6 @@
 import { FaUser } from "react-icons/fa";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
-import { ImageWithBlur } from "./image-with-blur";
 
 type UserCardProps = {
   user: {
@@ -20,12 +19,9 @@ export function UserCard({ user }: UserCardProps) {
     >
       <div className="hover:bg-secondary/40 p-4 rounded transition-all">
         <Avatar className="h-48 w-48 mb-2">
-          <ImageWithBlur
+          <AvatarImage
             src={user.image ?? ""}
             alt={`${user.name} avatar picture`}
-            className="aspect-square h-full w-full"
-            width={250}
-            height={250}
           />
           <AvatarFallback>
             <FaUser className="w-24 h-24" />
